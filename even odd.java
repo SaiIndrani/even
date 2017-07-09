f65e102de01a0bd7b55ea9566f840a0957e3eb3c
SaiIndrani <saiindrani_chintala@srmuniv.edu.in>
2017-07-09 12:45:56 +0530

    Add files via upload

---
+++ even odd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class I
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number;
+		Scanner s=new Scanner(System.in);
+		System.out.println("number");
+	if(number%2==0)
+	{
+		System.out.println("x is even");
+	}
+	else if(number%2!=0)
+	{
+	System.out.println("x is odd");
+	}
+	}
+}
